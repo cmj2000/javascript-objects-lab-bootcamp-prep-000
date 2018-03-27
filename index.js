5d548f13ae31d3865cc957949d50b33b8cc54769
c.mj2000@yahoo.com <c.mj2000@yahoo.com>
2018-03-27 00:35:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 function updateObjectWithKeyAndValue(object, key, value)
 {
   
-   newObj = Object.assign({}, object)
-  return newObj
+  return newObj = Object.assign({}, object);
+   
    
 }
