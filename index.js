951f70edf8999095b500597663a162dad2147ce9
c.mj2000@yahoo.com <c.mj2000@yahoo.com>
2018-03-27 00:57:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function updateObjectWithKeyAndValue(object, key, value)
 
 function  deleteFromObjectByKey(object, key) ////////////////
 {
- var newObj = Object.assign({}, obj)
+ var newObj = Object.assign({}, object)
  delete newObj.key
  return newObj
   
